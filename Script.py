1588fed8290d943a6f243974af52d76610000aab
Swarajjbot <165831838+Swarajjbot@users.noreply.github.com>
2024-04-26 17:51:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -201,11 +201,9 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>🔖[ @INDORECINEMAS5 ] : </b> <code>{file_name}</code>
+    CAPTION = """<b>[ @INDORECINEMAS5 ] : </b> <code>{file_name}</code>
 
-[ @INDORECINEMAS5 ] #𝙉𝙤𝟏  𝙈𝙤𝙫𝙞𝙚𝙨  𝘼𝙣𝙙  𝙒𝙚𝙗  𝙎𝙚𝙧𝙞𝙚𝙨  𝘾𝙝𝙖𝙣𝙣𝙚𝙡.🔖
-
-⏤͟͞𝙅𝙊𝙄𝙉 : <a href='https://t.me/INDORECINEMAS5'>ɪɴᴅᴏʀᴇ ᴄɪɴᴇᴍᴀꜱ</a></b>"""
+𝖩𝗈𝗂𝗇➥ : <a href='https://t.me/INDORECINEMAS5'>「𝙄𝙉𝘿𝙊𝙍𝙀 𝘾𝙄𝙉𝙀𝙈𝘼𝙎」</a></b>"""
 
 
     IMDB_TEMPLATE_TXT = """
