37be9f5458e5c066ef08ddad965a95d149f7c845
Swarajjbot <165831838+Swarajjbot@users.noreply.github.com>
2024-05-20 06:07:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = is_enabled((environ.get('USE_CAPTION_FILTER', 'True')), True)
 
 PICS = (environ.get('PICS', 'https://graph.org/file/f8406d2a3e3f4b04ccf4d.png https://graph.org/file/7e90948ced3eb29697772.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/9e07934327105cf8824b9.png")
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/1a234f99e7dc65c42ded4.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
