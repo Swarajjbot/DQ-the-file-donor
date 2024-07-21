70f7eee805d0b83cc3d7604b989161039e2dc9d7
Swarajjbot <165831838+Swarajjbot@users.noreply.github.com>
2024-07-21 21:09:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -202,8 +202,11 @@ Nᴀᴍᴇ - {}"""
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """[ @INDORECINEMAS5 ] 📂 <i><a href="https://t.me/INDORECINEMAS5">{file_name}</a></i>\n\n
+
+「 ⚜️@INDORECINEMAS5⚜️ 」
+「 ⚜️@INDORECINEMAS5⚜️ 」
     
-    𝖩𝗈𝗂𝗇➥ : <a href='https://t.me/INDORECINEMAS5'>"「𝙄𝙉𝘿𝙊𝙍𝙀 𝘾𝙄𝙉𝙀𝙈𝘼𝙎」"</a></b>"""
+𝖩𝗈𝗂𝗇➥ : <a href='https://t.me/INDORECINEMAS5'>"「𝙄𝙉𝘿𝙊𝙍𝙀 𝘾𝙄𝙉𝙀𝙈𝘼𝙎」"</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {query}
